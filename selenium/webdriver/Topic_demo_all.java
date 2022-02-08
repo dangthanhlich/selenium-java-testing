2ab8a32e3404207f8907ce9084a74cde99ad439d
Dang Thi Thanh Lich <thanhlichit1999@gmail.com>
2022-02-08 09:08:02 +0700

    Topic 8

---
+++ selenium/webdriver/Topic_demo_all.java
@@ -0,0 +1,52 @@
+package webdriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_demo_all {
+	//khai báo 1 biến đại diện cho selenium ưebdriver
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		//khởi tạo browser lên
+		driver = new FirefoxDriver();
+		//set thời gian chờ để tìm được element
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		//mở trang facebook lên
+		driver.get("https://www.facebook.com/");
+	}
+
+	@Test
+	public void TC_01() {
+		//ID 
+		driver.findElement(By.id("email")).sendKeys("thanhlichit1999@gmail.com");
+		sleepInSecond(3);
+		driver.findElement(By.id("pass")).sendKeys("1111");
+		sleepInSecond(3);
+	}
+
+	public void TC_02() {
+		
+	}
+
+	public void afterClass() {
+		driver.quit();
+	}
+	
+	public void sleepInSecond(long second) {
+		try {
+			Thread.sleep(second * 1000);
+		}catch(InterruptedException e){
+			e.printStackTrace();
+		}
+	}
+}
