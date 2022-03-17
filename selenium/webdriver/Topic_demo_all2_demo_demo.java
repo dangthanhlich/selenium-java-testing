2ef2499d7f690e68a796933413fa7eeca63fc803
Dang Thi Thanh Lich <thanhlichit1999@gmail.com>
2022-03-17 09:45:07 +0700

    topic26_upload_file1

---
+++ selenium/webdriver/Topic_demo_all2_demo_demo.java
@@ -0,0 +1,48 @@
+package webdriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_demo_all2_demo_demo {
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+	String osName = System.getProperty("os.name");
+	JavascriptExecutor jsExecutor;
+
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+	}
+
+	@Test
+	public void TC_01() {
+		
+	}
+	@Test
+	public void TC_02() {
+		
+	}
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+	
+	public void sleepInSecond(long second) {
+		try {
+			Thread.sleep(second * 1000);
+		}catch(InterruptedException e){
+			e.printStackTrace();
+		}
+	}
+}
